66197b346d3605f6f57b3d4cd9931468e6150095
quaeknas <quaeknas@web.de>
2020-02-01 20:20:31 +0100

    animation

---
+++ Assets/WeaponScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WeaponScript : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Gamepad.current.rightTrigger.isPressed) { animator.SetBool("Load", true); }
+        if (!Gamepad.current.rightTrigger.isPressed) { animator.SetBool("Load", false); }
+    }
+}
