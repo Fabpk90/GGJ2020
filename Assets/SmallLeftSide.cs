5fd71825a15f90a21b1924c2e8f649f6b1e2d37a
quaeknas <quaeknas@web.de>
2020-02-02 14:48:33 +0100

    SmallBoi is ready

---
+++ Assets/SmallLeftSide.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SmallLeftSide : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Animator animator;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("Shoot", false);
+        if (Gamepad.current.leftTrigger.isPressed)
+        {
+            animator.SetBool("Shoot", true);
+        }
+    }
+}
---
+++ Assets/SmallLegs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallLegs : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("Walking", true);
+        }
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("Walking", false);
+        }
+    }
+}
