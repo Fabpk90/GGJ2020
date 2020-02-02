2d6a0e01fef0448e0ad8c359f1c0e679cd30c3f7
quaeknas <quaeknas@web.de>
2020-02-02 11:36:15 +0100

    Pushback & Walking Animation

---
+++ Assets/Legs.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class Legs : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("Walking", true);
+        }
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("Walking", false);
+        }
+    }
+}
